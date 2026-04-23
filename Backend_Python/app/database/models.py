9b4b1d8102c069c675dbce65ab6051b0084eb3bb
Orest Sheremeta <orest.sheremeta@student.tuke.sk>
2026-04-23 17:39:18 +0200

    feat: integration db to endpoint

--- Backend_Python/app/database/models.py
+++ Backend_Python/app/database/models.py
@@ -1,4 +1,4 @@
-from database.database import db
+from app.database.database import db
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
 from sqlalchemy import String, Float, Integer, DateTime
--- Backend_Python/app/services/ingestion.py
+++ Backend_Python/app/services/ingestion.py
@@ -1,5 +1,5 @@
-from schemas.transaction import TransactionSchema
-from database.models import Transaction, TransactionEmbedding
+from app.schemas.parser_schemas import TransactionOut
+from app.database.models import Transaction, TransactionEmbedding
 from openai import OpenAI
 from sqlalchemy.dialects.postgresql import insert
 
@@ -9,24 +9,30 @@ class IngestionService:
         self.db_session = db_session
         self.client = OpenAI()
     
-    def ingest_transaction(self, schema: TransactionSchema):
-        transaction = insert(Transaction).values(
-            id=schema.id, 
-            time=schema.time, 
-            amount=schema.amount, 
-            description=schema.description,
-            mcc=schema.mcc,
-            counter_name=schema.counter_name,
-            category=schema.category
-            ).on_conflict_do_nothing(index_elements=['id'])
+    def ingest_transaction(self, schema: list[TransactionOut]):
+        descriptions_list = []
+        dumped_transactions = []
+
+        for _transaction in schema:
+            descriptions_list.append(_transaction.description)
+            dumped_transactions.append(_transaction.model_dump())
+
+        transaction = insert(Transaction).values(dumped_transactions).on_conflict_do_nothing(index_elements=['id'])
         
         response = self.client.embeddings.create(
-        input=f"{schema.description}",
+        input=descriptions_list,
         model="text-embedding-3-small"
         )
-        transaction_embedding = insert(TransactionEmbedding).values(transaction_id=schema.id, 
-                                                     embedding=response.data[0].embedding
-                                                     ).on_conflict_do_nothing(index_elements=['id'])
+        
+        embeddings_list = []
+        for i, _transaction in enumerate(response.data):
+            embeddings_list.append({
+                'transaction_id': schema[i].id,
+                'embedding': _transaction.embedding
+                
+            })
+        
+        transaction_embedding = insert(TransactionEmbedding).values(embeddings_list).on_conflict_do_nothing(index_elements=['id'])
         with self.db_session as session:
             session.execute(transaction)
             session.execute(transaction_embedding)
