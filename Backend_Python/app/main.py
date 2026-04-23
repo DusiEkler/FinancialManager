10edc65c21c5eeb8017f8afae68e1a6941fbd313
Orest Sheremeta <orest.sheremeta@student.tuke.sk>
2026-04-23 02:49:31 +0200

    feat: basic FastAPI setup. Endpoint for upload transactions

--- Backend_Python/app/main.py
+++ Backend_Python/app/main.py
@@ -1,1 +1,13 @@
-print("Hello World!@@!!!")  # TEST FOR LINTERS !"№"
+from fastapi import FastAPI
+from app.services.parser import parse_monobank_statement
+
+app = FastAPI()
+
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
+
+@app.post("/transactions/upload")
+async def upload_transactions(transactions_data: list[dict]):
+    count = len(parse_monobank_statement(transactions_data))
+    return {"parsed_count": count}
